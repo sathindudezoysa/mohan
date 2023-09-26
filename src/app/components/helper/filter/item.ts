1a809b9b007495a75900632c35f9acd898ea8e08
Sathindu Dhanushka <sathindu.d.zoysa@gmail.com>
2023-09-26 16:36:08 +0530

    done

--- src/app/components/helper/filter/item.ts
+++
@@ -1,7 +0,0 @@
-export interface Item {
-    id: string;
-    gridimg: string;
-    title: string;
-    shortdesc: string;
-    category: number[];
-}
--- src/app/components/models/contact/contact.ts
+++
@@ -1,9 +0,0 @@
-export class Contact {
-    id: number | undefined;
-    name: string | undefined;
-    email: string | undefined;
-    phone?: string | undefined;
-    subject: string | undefined;
-    message: string | undefined;
-    recaptcha: string | undefined;
-}
